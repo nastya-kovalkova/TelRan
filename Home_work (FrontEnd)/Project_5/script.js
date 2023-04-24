d0371974fbab0b75c2c49ea5c3f16e94d73118c4
nastya-kovalkova <nastyakovalkova333@gmail.com>
2023-04-24 13:00:38 +0300

    Project-5 and 4 tasks

---
+++ Home_work (FrontEnd)/Project_5/script.js
@@ -0,0 +1,44 @@
+// Программа №1
+let number = +prompt("Введите число");
+console.log(`10% от числа ${number} будет ${number * 0.1}`);
+
+
+// Программа №2
+// let number_1 = +prompt("Введите первое число");
+// let number_2 = +prompt("Введите второе число");
+
+// if (number_1 < number_2) {
+//     console.log(`Меньшее число -> ${number_1}`);
+// }
+// else if
+//     (number_1 > number_2) {
+//     console.log(`Меньшее число -> ${number_2}`);
+// }
+// else
+//     console.log("Числа равны");
+
+
+// Программа №3
+// let number = +prompt("Введите число");
+
+// if (number < 100) {
+//     console.log("Число меньше 100");
+// }
+// else if
+//     (number > 100) {
+//     console.log("Число больше 100");
+// }
+// else
+//     console.log("Число ровно 100");
+
+
+// Программа №4
+// let name = prompt("Как Вас зовут?");
+// let age = +prompt("Сколько Вам лет?");
+
+// if (age < 18) {
+//     console.log(`Hi, ${name}!`);
+// }
+// else {
+//     console.log(`Hello, ${name}!`);
+// }
